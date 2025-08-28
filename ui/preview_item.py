7e979350cf80b1ab4249bee228d99a5cd465f270
KQF <120025059+Inverated@users.noreply.github.com>
2025-08-28 17:49:49 +0800

    compact list

--- ui/preview_item.py
+++ ui/preview_item.py
@@ -6,18 +6,21 @@ from ui.image import PixMap
 
 class PreviewItem(QWidget):
     removeRequested = Signal(QWidget)
-    def __init__(self, page_no, document_name:str = None, image = None, curr_size:int = 100):
+    def __init__(self, page_no:int, document_name:str = None, document_page_range:list = None, image = None, curr_size:int = 100):
         super().__init__()   
         layout = QHBoxLayout()
         
         self.page_no = page_no
+        self.curr_size = curr_size
+        self.document_page_range = document_page_range
         self.document_name = document_name
+        self.image = image
 
         self.page_label = QLabel("Pg. " + str(page_no))
         
         self.image_label = QLabel()
-        self.pixmap = PixMap(image)
-        scaled = self.pixmap.scaled(curr_size)
+        self.pixmap = PixMap(self.image)
+        scaled = self.pixmap.scaled(self.curr_size)
         self.image_label.setPixmap(scaled)
         self.image_label.setFixedSize(scaled.size())
         
@@ -40,9 +43,14 @@ class PreviewItem(QWidget):
         self.page_label.setText("Pg. " + str(page_no))
         
     def update_image_size(self, new_size): 
+        self.curr_size = new_size
         scaled = self.pixmap.scaled(new_size)
         self.image_label.setPixmap(scaled)
         self.image_label.setFixedSize(scaled.size())
         
     def remove_clicked(self):
         return self.removeRequested.emit(self)
+    
+    def compact(self, next):
+        new_range = [self.document_page_range[0], next.document_page_range[1]] 
+        return PreviewItem(self.page_no, self.document_name, new_range, self.image, self.curr_size)
--- ui/preview_widget.py
+++ ui/preview_widget.py
@@ -15,13 +15,14 @@ class Preview(QWidget):
         scroll_area.setWidgetResizable(True)
         
         container = QWidget()
-        widget_stack = QVBoxLayout()
-        self.preview_item_list = [PreviewItem(page_no=i+1, curr_size=self.image_size, image='test/download' + str(i + 1) + '.jpg') for i in range(6)]
+        
+        self.widget_stack = QVBoxLayout()
+        self.preview_item_list = [PreviewItem(page_no=i+1, document_page_range=[i+1, i+1], curr_size=self.image_size, image='test/download' + str(i + 1) + '.jpg') for i in range(6)]
         
         for each in self.preview_item_list:
-            widget_stack.addWidget(each)
+            self.widget_stack.addWidget(each)
             
-        container.setLayout(widget_stack)
+        container.setLayout(self.widget_stack)
         scroll_area.setWidget(container)        
         items.addWidget(scroll_area)
 
@@ -35,9 +36,10 @@ class Preview(QWidget):
         size_slider = SizeSliderLayout(image_size=self.image_size)
         
         size_slider.connect(self.update_size)
-        for each in self.preview_item_list:
-            size_slider.connect(each.update_image_size)
-            each.removeRequested.connect(self.remove_page)
+        for i in range(self.widget_stack.count()):
+            preview_item_wid:PreviewItem = self.widget_stack.itemAt(i).widget()
+            size_slider.connect(preview_item_wid.update_image_size)
+            preview_item_wid.removeRequested.connect(self.remove_page)
         
         option_bar.addWidget(compact_checkbox)
         option_bar.addStretch()
@@ -52,9 +54,30 @@ class Preview(QWidget):
         
     def remove_page(self, page):
         page.setParent(None)
-        self.preview_item_list.remove(page)
-        for i in range(len(self.preview_item_list)):
-            self.preview_item_list[i].update_page_no(i + 1)
+        for i in range(self.widget_stack.count()):
+            temp:PreviewItem = self.widget_stack.itemAt(i).widget()
+            temp.update_page_no(i + 1)
             
-    def set_compact_view(self, is_compact):
-        print(is_compact)
+    def set_compact_view(self, compact):
+        if self.widget_stack.count() == 0:
+            return
+        
+        #0 not checked, 1 partially checked, 2 checked
+        if compact == 2:
+            start:PreviewItem = self.widget_stack.itemAt(0).widget()
+            new_stack:list[PreviewItem] = []
+            for i in range(1, self.widget_stack.count()):
+                curr_item:PreviewItem = self.widget_stack.itemAt(i).widget()
+                print(start.document_page_range)
+                print(curr_item.document_page_range)
+                print()
+                if curr_item.document_name == start.document_name and curr_item.document_page_range[0] == start.document_page_range[1] + 1:
+                    start = start.compact(curr_item)
+                    if i == self.widget_stack.count() - 1:
+                        new_stack.append(start)
+                else:
+                    new_stack.append(start)
+                    start = curr_item
+            print(new_stack)
+                
+                
